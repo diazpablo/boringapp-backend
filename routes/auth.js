f5d6585c3146feba73f1c9754ea06496bc27d41e
Pablo Diaz <diazpablo@gmail.com>
2020-09-18 14:00:51 +0200

    change status code for login rejection

--- routes/auth.js
+++ routes/auth.js
@@ -9,10 +9,10 @@ router.post('/', async (req, res) => {
 	if (error) return res.status(400).send(error.details[0].message);
 
 	let user = await User.findOne({ email: req.body.email });
-	if (!user) return res.status(400).send('Invalid email or password.');
+	if (!user) return res.status(401).send('Invalid email or password.');
 
 	const validPassword = await bcrypt.compare(req.body.password, user.password);
-	if (!validPassword) return res.status(400).send('Invalid email or password.');
+	if (!validPassword) return res.status(401).send('Invalid email or password.');
 
 	const token = user.generateAuthToken();
 	res.send(token);
